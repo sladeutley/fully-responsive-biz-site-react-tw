66f9c97dbe36ba872a161a7f29aa7bd42017fa50
sladeutley <slade.utley@gmail.com>
2023-01-01 07:47:34 -0800

    finished general structure for app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,32 @@ import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>hi</h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      {/* Note, if want to know what style does from 'styles', just command click it */}
+      <div className={`${styles.boxWidth}`}>
+        Navbar - placeholder for future components
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+    </div>
+
   </div>
 );
 
