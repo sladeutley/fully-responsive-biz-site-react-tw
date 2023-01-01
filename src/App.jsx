7c3097e60e852b00144b5240146d99e7df44f20a
sladeutley <slade.utley@gmail.com>
2023-01-01 07:35:45 -0800

    build structure of our app and import style file to make styling with tailwind a little easier by setting variables to long combined tailwind classnames we will use over and over

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./style";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <h1>hi</h1>
+  </div>
+);
 
-export default App
+export default App;
