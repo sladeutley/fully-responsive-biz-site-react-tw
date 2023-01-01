aba296387866ba5c12491b566c94066d2094081e
sladeutley <slade.utley@gmail.com>
2023-01-01 09:26:40 -0800

    create new useState for when menu for mobile should be open or closed and apply it to menu image in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     // NOTE, to know what these tailwind classes do, like below, go to tailwind docs and search for them - it will explain everything
     <nav className="w-full flex py-6 justify-between items-center navbar">
@@ -24,7 +26,13 @@ const Navbar = () => {
 
       {/* 38:00 - Mobile Devices - not sure the hidden thing seems counterintuitive to me. like above on desktop its hidden, but here on small devices its hidden - shouldn't it be the other way around */}
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img src={} />
+        <img 
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          // onClick={() => setToggle(!toggle)} //**in react, never want to update state using previous version of same old state. instead need to create a callback function like below. This way react knows to keep the state up to date
+          onClick={() => setToggle((prev) => !prev)} 
+        />
       </div>
 
     </nav>
