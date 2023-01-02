ff26ebd3f049ae84a73da215a38280c13eaf0a97
sladeutley <slade.utley@gmail.com>
2023-01-02 10:57:03 -0800

    started Business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const Business = () => {
-  return (
-    <div>Business</div>
-  )
-}
+const Business = () =>  (
+  // Below, classes of 'layout.section', etc are taking the already defined style from imported layout above - just less typing overall bc we reuse a bunch
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the business, <br className="sm:block hidden" /> we’ll handle
+        the money.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
 
-export default Business
+      <Button styles={`mt-10`} />
+      {/* Above, 'styles' is a prop that has style of margin top of 10 */}
+    </div>
+  </section>
+);
+
+export default Business;
