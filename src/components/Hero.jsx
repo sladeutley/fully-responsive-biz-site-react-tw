11f95db263c07617a2e17b43669dbdf62a2d12dd
sladeutley <slade.utley@gmail.com>
2023-01-02 09:32:44 -0800

    import GetStarted Component to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { discount, robot } from '../assets'
+import GetStarted from "./GetStarted"
 
 const Hero = () => (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
