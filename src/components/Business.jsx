ad93d1e72e0f69411151ab20c416800a094558e9
sladeutley <slade.utley@gmail.com>
2023-01-02 11:31:42 -0800

    create FeatureCard component to implement in Business component - done with Business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,6 +3,22 @@ import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
 
+const FeatureCard = ({ icon, title, content, index }) => ( //these are the props that we're passing below in '<FeatureCard />' at end of page, that we are getting from imported features above which we are mapping over for '<FeatureCard />' and are also destructured from '{...feature}'
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
 const Business = () =>  (
   // Below, classes of 'layout.section', etc are taking the already defined style from imported layout above - just less typing overall bc we reuse a bunch
   <section id="features" className={layout.section}>
@@ -23,8 +39,9 @@ const Business = () =>  (
 
     <div className={`${layout.sectionImg} flex-col`}>
       {features.map((feature, index) => (
-        <FeatureCard />
+        // <FeatureCard />
         // Above, bc we might want to reuse feature cards, but since we'll only be using it in this component, we create a FeatureCard component at top of this page
+        <FeatureCard key={feature.id} {...feature} index={index} /> //this is like how we did the <Button /> above, but we need key bc we're mapping over it, and we are spreading (aka '{...feature}) all of the feature properties so that it will contain all the features that are inside of feature object, and give it an index that is equal to index (see the property we are mapping over (featuress.map((feature,index))) )
       ))}
     </div>
   </section>
