8be5c5a5b4a4680721ece3c94af70b4f00c4d444
sladeutley <slade.utley@gmail.com>
2023-01-09 09:04:14 -0800

    adjusted get started button to not have margin left on small devices

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,8 @@ import styles from '../style'
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} ml-16 w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+  <div className={`${styles.flexCenter} sm:ml-16 w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    {/* Above, 'sm:ml-16' seems to mean margin left of 16 on every device except small devices, whereas I thought it would mean on small devices have margin left of 16, everything else no margin left. But it's working */}
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         {/* <p className="font-poppins font-medium text-[18px] leading-[23.4px] mr-2"> */}
